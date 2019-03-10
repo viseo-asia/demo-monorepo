fdb7d5e9dde24e4ddd7990b96a94016d6cb54053
Didier Hoarau <didier.hoarau.dev@gmail.com>
2019-03-10 16:00:54 +0800

    Python v3.1

--- demo-multitech/demo-python/src/app.py
+++ demo-multitech/demo-python/src/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "python @" + socket.gethostname() + " (Python)"
+    return "python @" + socket.gethostname() + " (Python v3)"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("80"))
